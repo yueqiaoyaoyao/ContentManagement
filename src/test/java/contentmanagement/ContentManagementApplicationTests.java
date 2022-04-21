ead665ca0ceed6d5f3f4f60c8857dc604884cfe1
yueqiao <19301136@bjtu.edu.cn>
2022-04-21 22:45:31 +0800

    create the project

---
+++ src/test/java/contentmanagement/ContentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package contentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
