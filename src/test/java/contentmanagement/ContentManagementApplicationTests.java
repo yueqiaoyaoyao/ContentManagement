b3fecbf5dd9ae3a8b48138b11d82dbd668421888
yueqiao <19301136@bjtu.edu.cn>
2022-04-21 23:12:18 +0800

    test Redis

--- src/test/java/contentmanagement/ContentManagementApplicationTests.java
+++ src/test/java/contentmanagement/ContentManagementApplicationTests.java
@@ -1,13 +1,26 @@
 package contentmanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class ContentManagementApplicationTests {
 
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testRedis(){
+        stringRedisTemplate.opsForValue().set("19301136", "zhou");
+        String res = stringRedisTemplate.opsForValue().get("19301136");
+        System.out.println(res);
     }
 
 }
